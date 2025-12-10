a27a6b4032c3d39e82cff68cb3ebda2d8ad76b06
Ariel Plaza <arielplazasalinas@gmail.com>
2025-12-10 16:15:54 -0300

    Se levanta base de proyecto con spring initializer se configura clase que ejecuta proyecto

---
+++ src/main/java/com/aluracursos/librosapi/LibrosapiApplication.java
@@ -0,0 +1,19 @@
+package com.aluracursos.librosapi;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibrosapiApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibrosapiApplication.class, args);
+	}
+
+    @Override
+    public void run(String... args) throws Exception {
+        //  Principal principal = new Principal();
+        //        principal.muestraElMenu();
+    }
+}
